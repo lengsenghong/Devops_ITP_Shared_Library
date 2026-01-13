7f8c9c64d1688ae97cb0ab255e6394fbbe7aa0d3
hong <hongsengleng4@gmail>
2026-01-14 00:34:03 +0700

    Fix reactjs  build path

--- vars/reactDockerfile.groovy
+++ vars/reactDockerfile.groovy
@@ -1,5 +1,10 @@
 def call() {
-    writeFile file: "Dockerfile", text: """
+    if (fileExists("Dockerfile")) {
+        echo "Dockerfile already exists — using repo Dockerfile"
+    } else {
+        echo "No Dockerfile found — generating from Shared Library"
+
+        writeFile file: "Dockerfile", text: """
 FROM node:18 AS build
 WORKDIR /app
 COPY . .
@@ -10,4 +15,5 @@ FROM nginx:alpine
 COPY --from=build /app/build /usr/share/nginx/html
 EXPOSE 80
 """
+    }
 }
