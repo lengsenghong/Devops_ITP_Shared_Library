0030c640d972beca0c446cec4df201b7a881c13f
hong <hongsengleng4@gmail>
2026-01-14 00:13:11 +0700

    fix dockerspring

--- vars/springDockerfile.groovy
+++ vars/springDockerfile.groovy
@@ -1,9 +1,15 @@
 def call() {
-    writeFile file: "Dockerfile", text: """
+    if (fileExists("Dockerfile")) {
+        echo "Dockerfile already exists — using repo Dockerfile"
+    } else {
+        echo "No Dockerfile found — generating from Shared Library"
+
+        writeFile file: "Dockerfile", text: """
 FROM eclipse-temurin:17-jdk-alpine
 WORKDIR /app
-COPY target/*.jar app.jar
+COPY backend/target/*.jar app.jar
 EXPOSE 8080
 ENTRYPOINT ["java","-jar","app.jar"]
 """
+    }
 }
