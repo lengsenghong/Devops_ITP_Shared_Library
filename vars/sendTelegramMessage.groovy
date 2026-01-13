bb28c1efe9f1d8580a328ef946c114d0dc6ac67b
hong <hongsengleng4@gmail>
2026-01-13 23:32:28 +0700

    fix

--- vars/sendTelegramMessage.groovy
+++ vars/sendTelegramMessage.groovy
@@ -1,8 +1,7 @@
 def call(String message, String token, String chatId) {
-    sh """
-    curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" \
-    -d chat_id=${chatId} \
-    -d text="${message}" \
-    -d parse_mode=MarkdownV2
-    """
+    sh '''
+    curl -s -X POST "https://api.telegram.org/bot'"$token"'/sendMessage" \
+    -d chat_id='"$chatId"' \
+    -d text="'"$message"'"
+    '''
 }
